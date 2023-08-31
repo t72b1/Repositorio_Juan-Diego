c0501844da0bf4cc4d0f99afaf6d206b557737fe
t72b1 <142433105+t72b1@users.noreply.github.com>
2023-08-30 19:10:11 -0500

    Delete Corte1/Sesion 7 directory

--- Corte1/Sesion 7/Fcn_Tupla.py
+++
@@ -1,7 +0,0 @@
-def app(*args,**kwargs):
-    print(args)
-    print(kwargs)
-
-
-if __name__=='__main__':
-    app()
--- Corte1/Sesion 7/aleatorio.py
+++
@@ -1,7 +0,0 @@
-import random
-def main():
-    a='0'
-    pal='David'
-    while a != 'exit':
-        pal+=(f'{a}')
-        a=input('Oprima Enter')
